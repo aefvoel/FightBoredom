1ca658e1f17aa0059b3158b750b1d76f3be27280
Toriq Wahid Syaefullah <toriqwahidsyaef@gmail.com>
2020-03-02 21:42:49 +0700

    Initial Commit

---
+++ singleviewapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  singleviewapp
+//
+//  Created by Toriq Wahid Syaefullah on 02/03/20.
+//  Copyright © 2020 Toriq Wahid Syaefullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
